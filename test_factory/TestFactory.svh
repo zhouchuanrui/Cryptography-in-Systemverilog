456a6c56fff064e8f35d4804a2bfbec1179f359d
MrZhou <zhouchuanrui@foxmail.com>
2017-05-02 21:51:53 +0800

    add listTests

--- test_factory/TestFactory.svh
+++ test_factory/TestFactory.svh
@@ -32,11 +32,24 @@ class TestFactory;
     static task run_test (string test_name);
         if (!tests.exists(test_name)) begin
             $display("Can't find %s in factory!!", test_name);
+            listTests();
             return;
         end
         tests[test_name].test();
     endtask: run_test
 
+    static function void listTests ();
+        string tn;
+        if(tests.first(tn)) begin
+            $display("Tests available: ");
+            do begin
+                $display("   %s", tn);
+            end while (tests.next(tn));
+        end
+        else begin
+            $display("No test in factory!!");
+        end
+    endfunction: listTests
 endclass
 
 `define __register(T) \
@@ -84,6 +97,7 @@ task factory_run_test ();
     end
     else begin
         $display("Please offer a +TEST=<test_name> in simulation arguments");
+        TestFactory::listTests();
     end
 endtask: factory_run_test
 
